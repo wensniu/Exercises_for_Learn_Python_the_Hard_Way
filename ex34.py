469e3d4f401b3cc619a435506162903a4d60132e
wensniu <whboy@WHBoys-MacBook-Pro.local>
2017-01-24 12:23:19 -0500

    0124Update

---
+++ ex34.py
@@ -0,0 +1,10 @@
+#Accessing Elements of Lists
+animals=['bear','python','peacock','kangaroo','whale','platypus']
+print animals[1]#the animal at 1
+print animals[2]#the 3rd animal
+print animals[0]#the 1st animal
+print animals[3]#the animal at 3
+print animals[4]#the 5th animal
+print animals[2]#the animal at 2
+print animals[5]#the 6th animal
+print animals[4]#the animal at 4
